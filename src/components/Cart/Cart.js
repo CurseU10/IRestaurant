e4fca60e018761da044a99d9873c8b6c4c34110d
Amrit Pandey <amritpandey382@gmail.com>
2024-01-11 13:38:39 +0530

    third commit

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -43,7 +43,7 @@ const Cart = (props) => {
             tableno : url.get('tableno'),
             served : 'false'
         }
-        await axios.post('https://087c-2405-201-3010-7be1-7c45-9302-ae72-e4b9.ngrok-free.app/menu/createOrder',payload);
+        await axios.post('https://fd9c-2409-4043-4e89-6f57-1db6-671c-2ab4-c5e4.ngrok-free.app/menu/createOrder',payload);
         cartCtx.clearall();
         setShowOrder(true);
     };
